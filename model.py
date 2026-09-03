37d0ae3ab1523d599d7d4bca66b6d073c00f9c3b
Artur <melnikarthur280912@gmail.com>
2026-09-03 14:37:22 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,21 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Ai(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(784, 512)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(512, 10)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        out = self.flatten(x)
-        out = self.fc1(out)
-        out = self.relu(out)
+        out = self.pool(F.relu(self.conv1(x)))
+        out = self.pool(F.relu(self.conv2(out)))
+        out = out.view(-1, 64 * 7 * 7)
+        out = F.relu(self.fc1(out))
         out = self.fc2(out)
 
         return out
